d1870dd24ab86faf1ae64593f37d2c54f354b0a9
Austin <ayce24@gmail.com>
2016-01-10 02:41:06 -0500

    enabled local file storage for logs, and disabled logging for tests

--- config/logger.js
+++ config/logger.js
@@ -3,30 +3,34 @@
 var winston = require('winston');
 winston.emitErrs = true;
 
-var logger = new winston.Logger({
-    transports: [
-	// FOR PRODUCTION LOGGING
-        // new winston.transports.File({
-        //     level: 'info',
-        //     filename: './logs/app.log',
-        //     handleExceptions: true,
-        //     json: false,
-        //     maxsize: 5242880, //5MB
-        //     maxFiles: 5,
-        //     colorize: false
-        // }),
-        new winston.transports.Console({
-            level: 'debug',
-            handleExceptions: true,
-            json: false,
-            prettyPrint: true,
-            silent: false,
-            timestamp: false,
-            colorize: true
-        })
-    ],
-    exitOnError: false
-});
+// TO DO : add mongodb transport to store logs
+
+// when not testing, log everything to console and local file
+if (process.env.NODE_ENV !== 'test') {
+    var logger = new winston.Logger({
+        transports: [
+            new winston.transports.File({
+                 level: 'info',
+                 filename: './logs/app.log',
+                 handleExceptions: true,
+                 json: false,
+                 maxsize: 5242880, //5MB
+                 maxFiles: 5,
+                 colorize: false
+             }),
+            new winston.transports.Console({
+                level: 'debug',
+                handleExceptions: true,
+                json: false,
+                prettyPrint: true,
+                silent: false,
+                timestamp: false,
+                colorize: true
+            })
+        ],
+        exitOnError: false
+    });
+}
 
 module.exports = logger;
 module.exports.stream = {
