4cbcd668cb84fed1be241f1df260ab1842079fc2
Prith Pal <prithpal87@gmail.com>
2016-01-31 22:44:45 -0500

    Rearrange files

---
+++ app/spa/src/app/areas/drivers/edit/driver-edit.js
@@ -0,0 +1,10 @@
+let Layout  = require('components/layout/layout');
+
+let DriversEdit = React.createClass({
+  render: () => {
+    return (
+      <Layout navCurrent='drivers'/>
+    )
+  }
+})
+module.exports = DriversEdit;
--- app/spa/src/app/areas/drivers/listing/drivers-listing.js
+++ app/spa/src/app/areas/drivers/listing/drivers-listing.js
@@ -1,4 +1,5 @@
-let Layout = require('components/layout/layout')
+let Layout  = require('components/layout/layout');
+let Table   = require('components/table/table');
 
 let TableActions = React.createClass({
   render() {
@@ -9,7 +10,7 @@ let TableActions = React.createClass({
   }
 })
 
-let Drivers = React.createClass({
+let DriversListing = React.createClass({
   initialState() {
     return {
       drivers: null
@@ -35,7 +36,7 @@ let Drivers = React.createClass({
       classNames: '',
       header: {
         left: 'Drivers',
-        right: <a className="btn success">New Driver</a>
+        right: <a id="new-driver" className="btn success" href="/dashboard/drivers/create"><i className="fa fa-plus"/>New Driver</a>
       },
       data: {
         columns: [
@@ -71,7 +72,7 @@ let Drivers = React.createClass({
             phone: `${_.random(0, 999)}-${_.random(0, 999)}-${_.random(0, 9999)}`,
             email: "woosh",
             active: (() => {
-              var active = !!_.random(1);
+              var active = id < 6;
               return (
                 <i className={cs({"fa fa-circle": true, active: active, inactive: !active})} />
               )
@@ -97,58 +98,7 @@ s            })(),
   }
 });
 
-let Table = React.createClass({
-  getDefaultProps() {
-    return {
-      classNames: '',
-      onColumnSorted: (columnKey, order = 'asc') => {}
-    };
-  },
-  render() {
-    var columns = this.props.data.columns;
 
-    return (
-      <div className={`widget table ${this.props.classNames}`} >
-        <div className="header grid">
-          <div className="col-1-2 left">{this.props.header.left}</div>
-          <div className="col-1-2 right">{this.props.header.right}</div>
-        </div>
-        <div className="data">
-          <div className="data-header">
-            {columns.map((column, index) => {
-              var column = columns[index];
-              return <span className={`column ${column.key}`} key={index} >{column.title}</span>
-            })}
-          </div>
-          <div className="data-items">
-            {this.props.data.items.map((item, index) => {
-              return (
-                <div className={`data-item ${index % 2 == 0? 'even' : 'odd'}`} key={index}>
-                  {columns.map((column, index) => {
-                    var column = columns[index];
-                    return <span className={`column ${column.key}`} key={index}>
-                      {(() => {
-                        var Component = column.component;
-                        if(Component){
-                          return <Component item={item}/>
-                        }
-                        return item[column.key] 
-                      })()}
-                    </span>
-                  })}
-                </div>
-              )
-            })}
-          </div>
-        </div>
-      </div>
-    )
-  }
-});
-
-
-
-
-module.exports = Drivers
+module.exports = DriversListing
 
 
