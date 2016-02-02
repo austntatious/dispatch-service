bfd1c6ae6f10ef5c722d6a54acc1381bf69ec12f
Austin <ayce24@gmail.com>
2016-02-02 15:50:56 -0500

    edited schema, added moment and uuid packages, and edited models

--- app/data/schema.js
+++ app/data/schema.js
@@ -1,16 +1,17 @@
 'use strict';
 
 // Schema to migrate up fresh database
-
+// To do: add index on tables
 var db = {
         workers: {
-            id: {type: 'increments', nullable: false, primary: true},
+            id: {type: 'serial', nullable: false, primary: true},
             uuid: {type: 'string', maxlength: 36, nullable: false, validations: {isUUID: true}},
             name: {type: 'string', maxlength: 150, nullable: false},
             status: {type: 'string', maxlength: 150, nullable: false, defaultTo: 'active'},
-            onDuty: {type: 'string', maxlength: 150, nullable: false, defaultTo: 'active'},
-            longitude: {type: 'decimal', precision: 9, scale: 6},
-            latitude: {type: 'decimal', precision: 9, scale: 6},
+            onDuty: {type: 'bool', nullable: false, defaultTo: false},
+            longitude: {type: 'decimal', precision: 9, scale: 6, nullable: true},
+            latitude: {type: 'decimal', precision: 8, scale: 6, nullable: true},
+            password: {type: 'string', maxlength: 60, nullable: false},
             organization_id: {type: 'integer', nullable: false, unsigned: true, references: 'organization.id'}, 
             meta_description: {type: 'string', maxlength: 200, nullable: true},
             created_at: {type: 'dateTime', nullable: false},
@@ -19,81 +20,65 @@ var db = {
             updated_by: {type: 'integer', nullable: true}
         },
         organizations: {
-            id: {type: 'increments', nullable: false, primary: true},
+            id: {type: 'serial', nullable: false, primary: true},
             uuid: {type: 'string', maxlength: 36, nullable: false, validations: {isUUID: true}},
             name: {type: 'string', maxlength: 150, nullable: false},
             email: {type: 'string', maxlength: 254, nullable: false, unique: true, validations: {isEmail: true}},
             website: {type: 'text', maxlength: 2000, nullable: true, validations: {isEmptyOrURL: true}},
             location: {type: 'text', maxlength: 65535, nullable: true},
-            status: {type: 'string', maxlength: 150, nullable: false, defaultTo: 'active'},
-            meta_description: {type: 'string', maxlength: 200, nullable: true},
+            status: {type: 'string', maxlength: 150, nullable: false, defaultTo: 'active'}
         },
         jobs: {
-            id: {type: 'increments', nullable: false, primary: true},
+            id: {type: 'serial', nullable: false, primary: true},
             uuid: {type: 'string', maxlength: 36, nullable: false, validations: {isUUID: true}},
             name: {type: 'string', maxlength: 150, nullable: false},
-            worker_id: {type: 'integer', nullable: false, unsigned: true, references: 'posts.id'},
+            worker_id: {type: 'integer', nullable: false, unsigned: true, references: 'worker.id'},
+            organization_id: {type: 'integer', nullable: false, unsigned: true, references: 'organization.id'},
             description: {type: 'string', maxlength: 200, nullable: true},
             type: {type: 'string', maxlength: 36, nullable: false}, // delivery or service
             notes: {type: 'text', maxlength: 65535, nullable: true},
             created_at: {type: 'dateTime',  nullable: false},
             created_by: {type: 'integer',  nullable: false},
             updated_at: {type: 'dateTime',  nullable: true},
-            updated_by: {type: 'integer',  nullable: true}
+            updated_by: {type: 'integer',  nullable: true},
+            assigned: {type: 'bool', nullable: false, defaultTo: false}
         },
         admins: {
-            id: {type: 'increments', nullable: false, primary: true},
+            id: {type: 'serial', nullable: false, primary: true},
             uuid: {type: 'string', maxlength: 36, nullable: false, validations: {isUUID: true}},
-            last_login: {type: 'dateTime', nullable: true}
+            name: {type: 'string', maxlength: 150, nullable: false},
+            status: {type: 'string', maxlength: 150, nullable: false, defaultTo: 'active'},
+            password: {type: 'string', maxlength: 60, nullable: false},
+            organization_id: {type: 'integer', nullable: false, unsigned: true, references: 'organization.id'},
+            last_login: {type: 'dateTime', nullable: true},
+            auto_assign: {type: 'bool', nullable: false, defaultTo: false}
         },
         stops: {
-            id: {type: 'increments', nullable: false, primary: true},
+            id: {type: 'serial', nullable: false, primary: true},
             uuid: {type: 'string', maxlength: 36, nullable: false, validations: {isUUID: true}},
-            status: {type: 'string', maxlength: 150, nullable: false, defaultTo: 'active'},
+            status: {type: 'string', maxlength: 150, nullable: false, defaultTo: 'active'}, // pending, cancelled
             type: {type: 'string', maxlength: 36, nullable: false}, // delivery or service
             longitude: {type: 'decimal', precision: 9, scale: 6},
-            latitude: {type: 'decimal', precision: 9, scale: 6},
+            latitude: {type: 'decimal', precision: 8, scale: 6},
             address: {type: 'text', maxlength: 2000, nullable: false},
-            job_id:
-            worker_id:
-            object_type: {type: 'string', maxlength: 150, nullable: false},
-            action_type: {type: 'string', maxlength: 150, nullable: false},
-            object_id: {type: 'integer', nullable: true},
+            job_id: {type: 'integer', nullable: false, unsigned: true, references: 'job.id'},
+            worker_id: {type: 'integer', nullable: false, unsigned: true, references: 'worker.id'},
             created_at: {type: 'dateTime', nullable: false},
             created_by: {type: 'integer', nullable: true},
             updated_at: {type: 'dateTime', nullable: true},
             updated_by: {type: 'integer', nullable: true},
             started_at: {type: 'dateTime', nullable: true},
-            completed_at: {type: 'dateTime', nullable: true}
+            completed_at: {type: 'dateTime', nullable: true},
+            start_by: {type: 'dateTime', nullable: true},
+            complete_by: {type: 'dateTime', nullable: true}
         },
         accounts: {
-            id: {type: 'increments', nullable: false, primary: true},
+            id: {type: 'serial', nullable: false, primary: true},
             user_id: {type: 'integer', nullable: false},
             permission_id: {type: 'integer', nullable: false}
         },
-        permissions_roles: {
-            id: {type: 'increments', nullable: false, primary: true},
-            role_id: {type: 'integer', nullable: false},
-            permission_id: {type: 'integer', nullable: false}
-        },
-        permissions_apps: {
-            id: {type: 'increments', nullable: false, primary: true},
-            app_id: {type: 'integer', nullable: false},
-            permission_id: {type: 'integer', nullable: false}
-        },
-        settings: {
-            id: {type: 'increments', nullable: false, primary: true},
-            uuid: {type: 'string', maxlength: 36, nullable: false, validations: {isUUID: true}},
-            key: {type: 'string', maxlength: 150, nullable: false, unique: true},
-            value: {type: 'text', maxlength: 65535, nullable: true},
-            type: {type: 'string', maxlength: 150, nullable: false, defaultTo: 'core', validations: {isIn: [['core', 'blog', 'theme', 'app', 'plugin', 'private']]}},
-            created_at: {type: 'dateTime', nullable: false},
-            created_by: {type: 'integer', nullable: false},
-            updated_at: {type: 'dateTime', nullable: true},
-            updated_by: {type: 'integer', nullable: true}
-        },
         tags: {
-            id: {type: 'increments', nullable: false, primary: true},
+            id: {type: 'serial', nullable: false, primary: true},
             uuid: {type: 'string', maxlength: 36, nullable: false, validations: {isUUID: true}},
             name: {type: 'string', maxlength: 150, nullable: false, validations: {matches: /^([^,]|$)/}},
             slug: {type: 'string', maxlength: 150, nullable: false, unique: true},
@@ -109,20 +94,20 @@ var db = {
             updated_by: {type: 'integer', nullable: true}
         },
         posts_tags: {
-            id: {type: 'increments', nullable: false, primary: true},
+            id: {type: 'serial', nullable: false, primary: true},
             post_id: {type: 'integer', nullable: false, unsigned: true, references: 'posts.id'},
             tag_id: {type: 'integer', nullable: false, unsigned: true, references: 'tags.id'},
             sort_order: {type: 'integer',  nullable: false, unsigned: true, defaultTo: 0}
         },
         accesstokens: {
-            id: {type: 'increments', nullable: false, primary: true},
+            id: {type: 'serial', nullable: false, primary: true},
             token: {type: 'string', nullable: false, unique: true},
             user_id: {type: 'integer', nullable: false, unsigned: true, references: 'users.id'},
             client_id: {type: 'integer', nullable: false, unsigned: true, references: 'clients.id'},
             expires: {type: 'bigInteger', nullable: false}
         },
         refreshtokens: {
-            id: {type: 'increments', nullable: false, primary: true},
+            id: {type: 'serial', nullable: false, primary: true},
             token: {type: 'string', nullable: false, unique: true},
             user_id: {type: 'integer', nullable: false, unsigned: true, references: 'users.id'},
             client_id: {type: 'integer', nullable: false, unsigned: true, references: 'clients.id'},
