c9c1f3bf11211e2d9cfb9a38d687a80ce814ae56
Austin <ayce24@gmail.com>
2016-02-03 04:01:11 -0500

    added event registry and event package to listen to new data events and publish --WIP

---
+++ app/events/index.js
@@ -0,0 +1,14 @@
+// register all app events and publish data to appropriate channels - pub/sub architecture
+
+var events  = require('events'),
+    util    = require('util'),
+    EventRegistry,
+    EventRegistryInstance;
+
+EventRegistry = function () {};
+util.inherits(EventRegistry, events.EventEmitter);
+
+EventRegistryInstance = new EventRegistry();
+EventRegistryInstance.setMaxListeners(100);
+
+module.exports = EventRegistryInstance;
